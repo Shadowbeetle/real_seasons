24ed868ca5102cd94f88df8acb0eb8752d79f41e
Shadowbeetle <tamas.kadlecsik@gmail.com>
2026-02-23 12:39:27 +0100

    default language based on browser's locale

--- lib/real_seasons_web/plugs/locale.ex
+++ lib/real_seasons_web/plugs/locale.ex
@@ -13,6 +13,7 @@ defmodule RealSeasonsWeb.Plugs.Locale do
     locale =
       conn.params["locale"] ||
         get_session(conn, :locale) ||
+        locale_from_header(conn) ||
         @default_locale
 
     locale = if locale in @supported_locales, do: locale, else: @default_locale
@@ -22,5 +23,25 @@ defmodule RealSeasonsWeb.Plugs.Locale do
     conn
     |> put_session(:locale, locale)
     |> assign(:locale, locale)
+  end
+
+  defp locale_from_header(conn) do
+    conn
+    |> get_req_header("accept-language")
+    |> List.first("")
+    |> parse_accept_language()
+    |> Enum.find(&(&1 in @supported_locales))
+  end
+
+  defp parse_accept_language(header) do
+    header
+    |> String.split(",")
+    |> Enum.map(fn part ->
+      case String.split(String.trim(part), ";") do
+        [lang | _] -> lang |> String.trim() |> String.split("-") |> List.first()
+        _ -> nil
+      end
+    end)
+    |> Enum.reject(&is_nil/1)
   end
 end

47e5e05e1e39b8d30cf0365087764588561deb26
Shadowbeetle <tamas.kadlecsik@gmail.com>
2026-02-24 16:52:27 +0100

    remove E flag

--- config/dev.exs
+++ config/dev.exs
@@ -48,12 +48,12 @@ config :real_seasons, RealSeasonsWeb.Endpoint,
     web_console_logger: true,
     patterns: [
       # Static assets, except user uploads
-      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$"E,
+      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$",
       # Gettext translations
-      ~r"priv/gettext/.*\.po$"E,
+      ~r"priv/gettext/.*\.po$",
       # Router, Controllers, LiveViews and LiveComponents
-      ~r"lib/real_seasons_web/router\.ex$"E,
-      ~r"lib/real_seasons_web/(controllers|live|components)/.*\.(ex|heex)$"E
+      ~r"lib/real_seasons_web/router\.ex$",
+      ~r"lib/real_seasons_web/(controllers|live|components)/.*\.(ex|heex)$"
     ]
   ]
 
